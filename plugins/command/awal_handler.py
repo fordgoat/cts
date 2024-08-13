bfcaccdc894657689390506e0b84660b2345cc03
fordgoat <adaadaajapan@gmail.com>
2024-08-13 23:01:49 +0700

    Update awal_handler.py

--- plugins/command/awal_handler.py
+++ plugins/command/awal_handler.py
@@ -8,7 +8,7 @@ async def start_handler(client: Client, msg: types.Message):
     first = msg.from_user.first_name
     last = msg.from_user.last_name
     fullname = first if not last else first + ' ' + last
-    username = '@reonisme' if not msg.from_user.username else '@' + msg.from_user.username
+    username = '@cpotid' if not msg.from_user.username else '@' + msg.from_user.username
     mention = msg.from_user.mention
     await msg.reply_text(
         text = config.start_msg.format(
@@ -90,12 +90,16 @@ async def gagal_kirim_handler(client: Client, msg: types.Message):
 async def help_handler(client, msg):
     db = Database(msg.from_user.id)
     member = db.get_data_pelanggan()
-    pesan = "Supported commands\n"
-    pesan += '/status — melihat status\n'
-    pesan += '\n— untuk mengirim pesan silahkan gunakan hastag dibawah:\n'
-    pesan += '#FwbBoy/#FwbGirl - untuk mencari teman, pacar, fwb dll\n'
-    pesan += '#FwbSpill - untuk spill sesuatu\n'
-    pesan += '\n#FwbStory - untuk bercerita\n'
+    pesan = "❏ Supported commands\n"
+    pesan += '├ • /status — melihat status\n'
+    pesan += '└ • /tf_coin — transfer coin ke user lain\n'
+    pesan += '\n— untuk mengirim pesan/foto & video gunakan hastag dibawah:\n'
+    pesan += '#Boy - untuk identitas cowo\n'
+    pesan += '#Girl - untuk identitas cewe\n'
+    pesan += '#Ask - digunakan untuk bertanya\n'
+    pesan += '#Spill - spill sesuatu\n'
+    pesan += '#Random - untuk kirim foto/video random\n'
+    pesan += '\n#Story - untuk bercerita\n'
     pesan += 'info pengaduan atau top up coin hubungi @othentix\n'
     if member.status == 'admin':
         pesan += '\nHanya Admin\n'
